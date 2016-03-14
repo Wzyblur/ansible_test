6f0cde3691b2c7bd7f957b85de45537ca8bcb910
piaotiejun <674216231@qq.com>
2016-03-14 17:32:32 +0800

    modify

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,6 @@ from app.flask_app import flask_app
 
 
 if __name__ == '__main__':
-    #run(app, host='0.0.0.0', port=10001, reloader=True)
-    run_simple('0.0.0.0', 10001, flask_app, use_reloader=True, use_debugger=True)
+    run(app, host='0.0.0.0', port=10001, reloader=True)
+    #run_simple('0.0.0.0', 10001, flask_app, use_reloader=True, use_debugger=True)
 
